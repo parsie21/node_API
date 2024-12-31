74267156711518367ea30265f9f6a05cfce238d3
Luca Parise <lucaparise100@gmail.com>
2024-12-31 15:41:31 +0100

    dasd

---
+++ luca/express/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+// express app
+const app = express(); 
+
+//register view engine 
+app.set('view engine', 'ejs');
+
+
+// listen for request 
+app.listen(3000);
+app.get('/index', (req,res) => {
+    //res.send('<p> Home page</p>');
+    res.sendFile('./views/index.html', {root:__dirname})
+});
+
+app.get('/about', (req,res) => {
+    //res.send('<p> About page</p>');
+    res.sendFile('./views/about.html', {root:__dirname})
+}); 
+
+// redirects 
+app.get('/about-us',(req,res) => {
+    res.redirect('./about')
+})
+
+// 404 page
+app.use((req,res)=> {
+    res.status(404);
+    res.sendFile('./views/404.html', {root:__dirname})
+})
+
