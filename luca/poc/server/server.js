0d7cb9ccd5a05ec055ecee4f5478d6aff9542851
Marco Perazzolo <perazzolopera@gmail.com>
2025-01-03 21:26:41 +0100

    Modificato nome comando invio segnale
    Da sendSignalToServer a SendRequirementToOllama

--- luca/poc/server/server.js
+++ luca/poc/server/server.js
@@ -15,7 +15,7 @@ app.listen(expressPort, () => {
 
 
 // Ollama configuration
-const ollamaServer = '192.168.1.231';
+const ollamaServer = 'localhost';
 const ollamaPort = '11434';
 const model = 'llama3.2:latest';
 const requirementsContext = `Analizza il seguente requisito software e valuta la sua semantica.
@@ -66,6 +66,48 @@ app.post('/analyzeRequirement', async (req, res) => {
     }
 });
 
+const generateCodeContext = `Analizza il seguente requisito software e valuta la sua semantica.
+                            Rispondi esclusivamente con un file JSON nel seguente formato:
+                                {
+                                    "passed": <true/false>,
+                                    "suggestions": [
+                                        "Suggerimento 1",
+                                        "Suggerimento 2",
+                                        ...
+                                    ]
+                                }
+                            Regole di valutazione:
+                            - Il requisito deve essere chiaro, completo, non ambiguo, e tecnicamente corretto.
+                            - Segnala "passed: false" se il requisito è vago, incompleto, ambiguo, o non corretto.
+                            - Nella lista "suggestions", specifica come migliorare il requisito (es. chiarire dettagli, rimuovere ambiguità, completare informazioni mancanti).
+                            
+                            Sii molto critico e stringente, sei un programmatore che deve valutare i requisiti che deve implementare nel codice per evitare che siano ambigui.
+                            Non rispondere a caso se non riesci a valutare efficacemente il requisito, se hai dubbi o sei insicuro, dai passed:false
+                            Requisito:`;
+                            
+app.post('/generateCode', async (req, res) => {
+    try {
+        console.log("/generateCode endpoint called");
+        console.log('Signal received:', requirement);
 
+        // Call Ollama API
+        const ollamaResponse = await axios.post(
+            `http://${ollamaServer}:${ollamaPort}/api/generate`,
+            {
+                model: model,
+                system: generateCodeContext,
+                prompt: code,
+                stream: false
+            }
+        );
+        //Print Ollama response
+        console.log('Ollama response:', ollamaResponse.data.response);
+        res.status(200).json({ ollamaResponse: ollamaResponse.data.response });
+
+    } catch (error) {
+        console.error('Error during Ollama API call:', error.message);
+        res.status(500).json({ error: 'Failed to process the requirement' });
+    }
+});
 // Middleware to parse JSON
 app.use(express.json());
--- luca/poc/signal/extension.js
+++ luca/poc/signal/extension.js
@@ -9,19 +9,24 @@ const axios = require('axios')
  */
 function activate(context) {
 
-	console.log('Your extension "ExtensionSignal" is now active!');
+	console.log('Your extension "sendSignal" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('sendSignal.sendSignalToServer', function () {
+	const requirementToOllamaCommand = vscode.commands.registerCommand('sendSignal.sendRequirementToOllama', function () {
 		// The code you place here will be executed every time your command is executed
-		sendSignal();
+		sendRequirementToOllama();
 	});
-
+	const generateCodeWithOllamaCommand = vscode.commands.registerCommand('sendSignal.generateCodeWithOllama', function () {
+		// The code you place here will be executed every time your command is executed
+		generateCodeWithOllama();
+	});
 	
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(requirementToOllamaCommand);
+	context.subscriptions.push(generateCodeWithOllamaCommand);
+
 }
 
 function deactivate() {}
@@ -31,9 +36,25 @@ var server = require('../server/server.js');
 var expressPort = server.expressPort;
 var expressServer = server.expressServer;
 
-async function sendSignal() {
+async function sendRequirementToOllama() {
 	try {
 		const response = await axios.post('http://'+expressServer+':'+expressPort+'/analyzeRequirement', {
+			source : 'vscode-extension',
+			timestamp: new Date().toISOString(),
+		})
+
+		console.log('Signal Sent Successfully', response.data);
+		vscode.window.showInformationMessage('Signal Sent Successfully');
+		
+	} catch (error) {
+		console.error('Error sending signal', error);
+		vscode.window.showErrorMessage('Error sending signal');
+	}
+}
+
+async function generateCodeWithOllama() {
+	try {
+		const response = await axios.post('http://'+expressServer+':'+expressPort+'/generateCode', {
 			source : 'vscode-extension',
 			timestamp: new Date().toISOString(),
 		})
